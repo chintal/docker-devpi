8dea7f4d5edf64fec87e65dad0c3a1160a4a308e
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-03-27 00:08:51 +0530

    Renable root pypi mirror

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if ! [ -d $DEVPI_PATH/server ]; then
 	echo "First Run. Initializing devpi."
-	devpi-init --no-root-pypi \
+	devpi-init \
 		--role standalone \
 		--root-passwd $DEVPI_ROOT_PASSWORD \
 		--serverdir $DEVPI_PATH/server
